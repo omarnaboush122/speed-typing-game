981b9b89fa3ea3207e7ca30ab8bbc1a73338b15c
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-08 18:15:59 -0800

    create calculate word count function

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ function App() {
     const { value } = e.target;
     setText(value);
   };
+
+  const calculateWordCount = () => {
+    const wordArray = text.trim().split(" ");
+    return wordArray.filter(word => word !== "").length;
+  }
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
       <h4>Time remaining: ???</h4>
-      <button>Start</button>
+      <button onClick={console.log(calculateWordCount())}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
