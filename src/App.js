7d11654c91a111b7ad69b221c3feb163b5201352
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-08 19:53:34 -0800

    create startgame function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState(5);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
-  const [wordCount,setWordCount] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
 
   useEffect(() => {
     if (isTimeRunning && timeRemaining > 0) {
@@ -28,12 +28,18 @@ function App() {
     return wordArray.filter((word) => word !== "").length;
   };
 
+  function startGame() {
+    setIsTimeRunning(true);
+    setTimeRemaining(5);
+    setText("");
+  }
+
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={() => setIsTimeRunning(true)}>Start</button>
+      <button onClick={startGame}>Start</button>
       <h1>Word count: {wordCount} </h1>
     </div>
   );
